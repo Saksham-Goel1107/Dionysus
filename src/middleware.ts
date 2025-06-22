eafc358791971e858889796a435143650751e6a7
Saksham Goel <sakshamgoel1107@gmail.com>
2025-06-22 16:25:34 +0530

    Upgraded the packages versions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/api/stripe/webhook(.*)", "/", "/sync-user(.*)", "/docs(.*)", "/privacy(.*)", "/terms(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)",  "/", "/docs(.*)", "/privacy(.*)", "/terms(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
