0bd6dcb4d9d8d14c73336c8e21377c717cea0098
Saksham Goel <sakshamgoel1107@gmail.com>
2025-06-29 12:16:27 +0530

    Trying to add and test googleonetap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
-
+import { GoogleOneTap } from '@clerk/nextjs'
 import { Features } from "./components/features";
 import { Footer } from "./components/footer";
 import { Hero } from "./components/hero";
 import { HowItWorks } from "./components/how-it-works";
 import { Navbar } from "./components/navbar";
 
-export default async function Home() {  
-  
+export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <main>
-        <Hero />
-        <Features />
-        <HowItWorks />
-      </main>
-      <Footer />
-    </div>
-  )
+    <>
+      <GoogleOneTap
+        cancelOnTapOutside={true}
+        itpSupport={true}
+        fedCmSupport={true}
+      />
+      <div className="min-h-screen bg-background">
+        <Navbar />
+        <main>
+          <Hero />
+          <Features />
+          <HowItWorks />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
 }
