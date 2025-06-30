d235a7af48c7f7176afcf8fedfff088d81d68125
Saksham Goel <sakshamgoel1107@gmail.com>
2025-06-30 16:35:27 +0530

    Basic Templete Done

--- src/app/(protected)/ct/page.tsx
+++ src/app/(protected)/ct/page.tsx
@@ -1,14 +1,28 @@
 "use client"
-import { useState, useEffect } from 'react';
-import 'stream-chat-react/dist/css/v2/index.css';
-import './layout.css';
-import type { User, Channel as StreamChannel } from 'stream-chat';
-import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import { useEffect } from 'react';
+import type { Attachment as AttachmentType, ChannelFilters, ChannelSort, User } from 'stream-chat';
+import {
+  Attachment,
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+  useCreateChatClient,
+  type AttachmentProps,
+} from 'stream-chat-react';
+import { EmojiPicker } from 'stream-chat-react/emojis';
+import { init, SearchIndex } from 'emoji-mart';
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY || '';
-const userId = process.env.NEXT_PUBLIC_STREAM_USER_ID || '';
-const userName = process.env.NEXT_PUBLIC_STREAM_USER_NAME || '';
-const userToken = process.env.NEXT_PUBLIC_STREAM_USER_TOKEN || '';
+import './layout.ct.css';
+
+const apiKey = 'dz5f4d5kzrue';
+const userId = 'bold-poetry-3';
+const userName = 'bold';
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYm9sZC1wb2V0cnktMyIsImV4cCI6MTc1MTI5ODMxN30.XJb6HuCmrOz-d9o6CxE8TE2n9NAap8Iye1gZBQxx_ZU';
 
 const user: User = {
   id: userId,
@@ -16,8 +30,42 @@ const user: User = {
   image: `https://getstream.io/random_png/?name=${userName}`,
 };
 
+const sort: ChannelSort = { last_message_at: -1 };
+const filters: ChannelFilters = {
+  type: 'messaging',
+  members: { $in: [userId] },
+};
+
+const attachments: AttachmentType[] = [
+  {
+    image:
+      'https://images-na.ssl-images-amazon.com/images/I/71k0cry-ceL._SL1500_.jpg',
+    name: 'iPhone',
+    type: 'product',
+    url: 'https://goo.gl/ppFmcR',
+  },
+];
+
+const CustomAttachment = (props: AttachmentProps) => {
+  const { attachments } = props;
+  const [attachment] = (attachments || []);
+  if (attachment?.type === 'product') {
+    return (
+      <div>
+        Product:
+        <a href={attachment.url} rel="noreferrer">
+          <img alt="custom-attachment" height="100px" src={attachment.image} />
+          <br />
+          {attachment.name}
+        </a>
+      </div>
+    );
+  }
+
+  return <Attachment {...props} />;
+};
+
 const App = () => {
-  const [channel, setChannel] = useState<StreamChannel>();
   const client = useCreateChatClient({
     apiKey,
     tokenOrProvider: userToken,
@@ -27,20 +75,28 @@ const App = () => {
   useEffect(() => {
     if (!client) return;
 
-    const channel = client.channel('messaging', 'custom_channel_id', {
-      image: 'https://getstream.io/random_png/?name=react',
-      name: 'Talk about React',
-      members: [userId],
-    });
+    const initAttachmentMessage = async () => {
+      const [channel] = await client.queryChannels(filters, sort);
 
-    setChannel(channel);
+      if (channel) {
+        await channel.sendMessage({
+          text: 'Your selected product is out of stock, would you like to select one of these alternatives?',
+          attachments,
+        });
+      }
+    };
+
+    initAttachmentMessage().catch((error) => {
+      console.error(`Failed to initialize attachments`, error);
+    });
   }, [client]);
 
   if (!client) return <div>Setting up client & connection...</div>;
 
   return (
     <Chat client={client}>
-      <Channel channel={channel}>
+      <ChannelList filters={filters} sort={sort} />
+      <Channel Attachment={CustomAttachment} EmojiPicker={EmojiPicker} emojiSearchIndex={SearchIndex}>
         <Window>
           <ChannelHeader />
           <MessageList />
--- src/app/(protected)/ct/stream-chat.d.ts
+++ src/app/(protected)/ct/stream-chat.d.ts
@@ -1,6 +1,12 @@
-import { DefaultChannelData } from 'stream-chat-react';
+import { DefaultAttachmentData, DefaultChannelData } from 'stream-chat-react';
 
 declare module 'stream-chat' {
+  interface CustomAttachmentData extends DefaultAttachmentData {
+    image?: string;
+    name?: string;
+    url?: string;
+  }
+
   interface CustomChannelData extends DefaultChannelData {
     image?: string;
   }
