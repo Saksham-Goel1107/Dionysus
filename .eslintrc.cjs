068723e29b3bd53eb14f4c98ae2e6651da910423
Saksham-Goel1177 <sakshamop1107@gmail.com>
2025-07-17 22:51:51 +0530

    Testing never going to merge

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,1 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true
-  },
-  plugins: [
-    "@typescript-eslint"
-  ],
-  extends: [
-    "next",
-    "next/core-web-vitals"
-  ],
-};
-module.exports = config;
+
