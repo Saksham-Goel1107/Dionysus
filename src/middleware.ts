5fe78be6a25ab59bfe225d2745558afd1798c0c8
Saksham Goel <sakshamgoel1107@gmail.com>
2025-08-01 14:21:00 +0530

    fix: more added security as suggested by pentest tools but still not removed unsafe eval and unsafe inline a security issue but will fix in later updates as removing it may break the code

--- src/middleware.ts
+++ src/middleware.ts
@@ -217,6 +217,9 @@ export default clerkMiddleware(async (auth, request) => {
   response.headers.set('Cross-Origin-Resource-Policy', 'same-origin');
   response.headers.set('X-Content-Type-Options', 'nosniff');
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+  response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-XSS-Protection', '1; mode=block');
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
 
   return response;
 });
